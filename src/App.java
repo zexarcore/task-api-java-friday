41fa8eb3252fd46761d89dec84329f42abe28e22
jcortes <jc.cortes010@gmail.com>
2025-07-25 21:04:26 -0500

    feat(global): started project

---
+++ src/App.java
@@ -0,0 +1,18 @@
+
+import tasks.models.Task;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Task task = new Task("Encender", "Encender el TV", false);
+        System.out.println("Tarea Pendiente:" + 
+        task.getName() + 
+        "Descripcion:" + 
+        task.getDescription() + 
+        task.getSatus() );
+
+        task.setStatus(true);
+        
+        System.out.println("Tarea Finalizada:" + task.getName() + "Descripcion:" + task.getDescription()+ task.getSatus() );
+    }
+}
---
+++ src/tasks/models/Task.java
@@ -0,0 +1,39 @@
+package tasks.models;
+
+public class Task {
+    private String name;
+    private String description;
+    private boolean status;
+
+    public Task(String name, String description, boolean status){
+        this.name = name;
+        this.description = description;
+        this.status = status;
+    }
+
+    // Setters
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public void setStatus(boolean status){
+        this.status = status;
+    }
+
+    // Getters
+    public String getName(){
+        return this.name;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public boolean getSatus(){
+        return this.status;
+    }
+}
