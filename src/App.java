514cd3c31a06cdce4a6153a90d1fc852ecbe7eda
jcortes <jc.cortes010@gmail.com>
2025-08-29 20:49:45 -0500

    feat(task): implement CRUD operations in TaskUseCase and update App for task management

--- src/App.java
+++ src/App.java
@@ -1,18 +1,46 @@
 
 import tasks.models.Task;
+import tasks.useCases.TaskUseCase;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        Task task = new Task("Encender", "Encender el TV", false);
-        System.out.println("Tarea Pendiente:" + 
-        task.getName() + 
-        "Descripcion:" + 
-        task.getDescription() + 
-        task.getSatus() );
+        TaskUseCase taskUseCase = new TaskUseCase();
 
-        task.setStatus(true);
-        
-        System.out.println("Tarea Finalizada:" + task.getName() + "Descripcion:" + task.getDescription()+ task.getSatus() );
+        // Create
+
+        System.out.println("Se procede a crear las tareas...");
+        String create = taskUseCase.create("Leer la documentacion.", "Revisar la guia de conceptos de POO" );
+        // System.out.println(create);
+
+        String create2 = taskUseCase.create("Evaluar la documentacion.", "Se procede a evualuar los conceptos aprendidos." );
+        // System.out.println(create2);
+        System.out.println("\n\n");
+
+        System.out.println("Consultar todas las tareas...");
+        // Read
+        String read = taskUseCase.all();
+        System.out.println(read);
+        System.out.println("\n\n");
+
+        System.out.println("Se ha consultado la tarea con el indice 1");
+         // Read find by index
+        String read2 = taskUseCase.findByIndex(1);
+        System.out.println(read2);
+        System.out.println("\n\n");
+
+
+        System.out.println("Actualizamos la tarea con indice 0.");
+        // Update
+        String update = taskUseCase.update(0, "Re-Leer documentacion", "Se ha leido la documentacion varias veces.", true);
+        System.out.println(update);
+
+        System.out.println("\n\n");
+
+
+        System.out.println("Eliminamos la tarea con indice 1.");
+        // Delete
+        String delete = taskUseCase.delete(1);
+        System.out.println(delete);
     }
 }
--- src/tasks/useCases/TaskUseCase.java
+++ src/tasks/useCases/TaskUseCase.java
@@ -6,7 +6,7 @@ import java.util.List;
 import tasks.models.Task;
 
 // Contenedor de metodos y atributos
-public class TaskUseCase {
+public class TaskUseCase { // Clase de las acciones a realizar el Use Case
 
     // Propiedad
     private final List<Task> tasks; // Definicion Lista en memoria
@@ -16,7 +16,47 @@ public class TaskUseCase {
         this.tasks = new ArrayList<>(); // Inicializacion - Preparar el uso de la lista.
     }
 
-    // CREATE
+    // Read
+    public String all(){
+        try {
+            StringBuilder result = new StringBuilder(); // preparando la concatenacion de los strings.
+            for (int i = 0; i < tasks.size(); i++) { // Se procede a concatenar las tareas al string.
+                Task task = tasks.get(i); // Se obtiene la tarea.
+                result.append("Tarea ")
+                .append(i)
+                .append(": ")
+                .append(task.getName())
+                .append(" Descripcion: ")
+                .append(task.getDescription())
+                .append("\n"); // Concatenar la tarea al string.
+            }
+
+            return result.toString(); // Retornando la concatenacion de las tareas.
+            
+        } catch (Exception e) {
+           return "No ha sido posible listar las tareas.";
+        }
+    }
+
+    // Read by index
+    public String findByIndex(int index){
+        try {
+             Task taskFound = new Task(); // Todavia no se ha encontrado
+            for (int i = 0; i < tasks.size(); i++) { // Se procede a buscar
+                if(index == i){ // Se aplica el filtro o condicional
+                    Task task = tasks.get(index); // Se obtiene la tarea encontrada
+                    taskFound = task; // Se asigna la tarea encontrada
+                }
+            }
+            // Se imprime la tarea encontrada.
+            return "Tarea Encontrada:" + taskFound.getName() + " Descripcion:" + taskFound.getDescription();
+        }
+        catch (Exception e) {
+            return "no ha sido posible encontrar la tarea.";
+        }
+    }
+
+    // Create
     public String create(String name, String description) {
         try {
             Task task = new Task(name, description, false);
@@ -26,5 +66,35 @@ public class TaskUseCase {
         catch (Exception e) {
             return "Ha ocurrido un error, por favor intentelo nuevo";
         }
+    }
+
+    // Update
+    public String update(int index, String name, String description, boolean status) {
+        try {
+            Task taskFound = new Task();
+            for (int i = 0; i < tasks.size(); i++) {
+               if(index == i){
+                Task task = tasks.get(index);
+                task.setName(name);
+                task.setDescription(description);
+                task.setStatus(status);
+                taskFound = task;
+               }
+            }
+            return "Tarea Actualizada:" + taskFound.getName() + " Descripcion:" + taskFound.getDescription();
+        }
+        catch (Exception e) {
+            return "Ha ocurrido un error, por favor intentelo nuevo";
+        }
+    }
+
+    // Delete
+    public String delete(int index){
+       try {
+            tasks.remove(index);
+            return "Se ha eliminado la tarea correctamente.";
+       } catch (Exception e) {
+            return "no ha sido posible eliminar la tarea.";
+       }
     }
 }
